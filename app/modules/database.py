411ba1212b1e2b81a2737269c4e5f137d721b55f
mbc <chanhoobang37@gmail.com>
2024-05-23 12:45:43 +0900

    database.py 수정, draw_map.py 수정

--- app/modules/database.py
+++ app/modules/database.py
@@ -1,12 +1,11 @@
 from database import CONNECTION
 
-
-def select(table_name):
+def select(table_name, date):
     try:
         conn = CONNECTION
         
         cursor = conn.cursor()
-        sql = f"select * from {table_name}"
+        sql = f"SELECT * FROM {table_name} WHERE date = {date}"
         cursor.execute(sql)
 
         data = cursor.fetchall()
--- app/modules/draw_map.py
+++ app/modules/draw_map.py
@@ -1,6 +1,7 @@
 import os
 import folium
 import MarkerCluster
+from modules import database
 
 # 국내 확진자 수를 그리는 지도
 # date: 지도를 그릴 날짜 데이터
@@ -8,31 +9,32 @@ import MarkerCluster
 def draw_map(date, type):
     map1 = folium.Map(location=[37, 127], locatzoom_start=10, zoom_control=True, control_scale=True)
 
-    if type == 
-
     cluster = MarkerCluster().add_to(map1)
 
-    date = "2023-04-01"
-    list = data["city_name"].unique().tolist()
+    if type == "korea" | type == "global":
+        data_list = database.select(type, date)
+    else:
+        raise ValueError("올바르지않은 접근입니다!")
 
-    for el in list:
-        filter_date = data["date"] == date
+    for data in data_list:
         
-        temp_lat = get_xy(el)[0]
-        temp_lon = get_xy(el)[1]
+        temp_lat = data[위도]
+        temp_lon = data[경도]
         
         temp_location = [temp_lat, temp_lon]
         
-        name = change_city_name(el)
+        name = data[이름]
         
         content = f"""
-        <h3>시군구: {name}</h3>
-        <p>확진자 수: {data[filter_date].loc[data["city_name"]==el]["confirmed_cases"].values[0]}</p>
-        <p>사망자 수: {data[filter_date].loc[data["city_name"]==el]["death_cases"].values[0]}</p>
+        <h3>지명: {name}</h3>
+        <p>확진자 수: {data[]}</p>
+        <p>사망자 수: {data[]}</p>
+        <p>재감염자 수: {data[]}</p>
+        <p>재감염률: {data[]}</p>
         <p>날짜: {date}</p>
         """
         temp_c = folium.Circle(temp_location,
-                            radius = data[filter_date].loc[data["city_name"]==el]["confirmed_cases"].values[0]/1000,
+                            radius = data[확진자 수]["confirmed_cases"].values[0]/1000,
                             popup=folium.Popup(content, max_width=200))
         temp_c.add_to(cluster)
     file_path = "./templates/covid19/covid19_map.html"
